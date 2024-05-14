18bb015e0b9f20d3687211644b2601e120532b3a
Janaina-Costa <janainacosta.dev@gmail.com>
2024-05-13 21:28:52 -0300

    adiciona endpoints de tela adiciona importação da fonte

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      mobile: "375px",
+      tablet: "768px",
+      desktop: "1440px",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
