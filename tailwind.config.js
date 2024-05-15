889eff53f49604ef4e23c9d74bd4003bb6b63821
Janaina-Costa <janainacosta.dev@gmail.com>
2024-05-15 20:53:54 -0300

    ajusta breackpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      'tablet': "376px",
-      'desktop': "768px",
+      tablet: "480px",
+      desktop: "1044px",
     },
     extend: {
       backgroundImage: {
-        "mobile-light-bg":"url(./assets/pattern-background-mobile-light.svg)",
-        "tablet-light-bg":"url(./assets/pattern-background-tablet-light.svg)",
-        "desktop-light-bg":"url(./assets/pattern-background-desktop-light.svg)",
-
+        "mobile-light-bg": "url(./assets/pattern-background-mobile-light.svg)",
+        "tablet-light-bg": "url(./assets/pattern-background-tablet-light.svg)",
+        "desktop-light-bg":
+          "url(./assets/pattern-background-desktop-light.svg)",
       },
     },
   },
